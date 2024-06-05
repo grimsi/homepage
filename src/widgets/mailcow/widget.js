3a359011d01b4bdac45eabd74d0db4ffed4d6f7a
grimsi <9295182+grimsi@users.noreply.github.com>
2024-06-05 16:02:05 +0200

    Fix for Mailcow widget (see #3578 for similiar issue)

--- src/widgets/mailcow/widget.js
+++ src/widgets/mailcow/widget.js
@@ -1,8 +1,14 @@
 import credentialedProxyHandler from "../../utils/proxy/handlers/credentialed";
 
 const widget = {
-  api: "{url}/api/v1/get/domain/all",
+  api: "{url}/api/v1/get/{endpoint}",
   proxyHandler: credentialedProxyHandler,
+
+  mappings: {
+    domains: {
+      endpoint: "domain/all",
+    },
+  },
 };
 
 export default widget;
